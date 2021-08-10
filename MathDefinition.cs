f740d924d521ebb253731d0b3cff1c905372f589
Simon Tamás <simitomi2003@gmail.com>
2021-08-11 00:25:03 +0200

    Create MathDefinition.cs

---
+++ MathDefinition.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace Trigonometrics
+{
+    class MathDefinition
+    {
+        public string Name { get; set; }
+        public bool Enabled { get; set; }
+        public bool CanBeShown { get; set; }
+        public Func<double, double> Result { get; set; }
+        public Func<double, double, double> MyProperty { get; set; }
+        public Brush Brush { get; set; }
+    }
+}
